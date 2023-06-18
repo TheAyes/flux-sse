b45cfd495fe8b35b87e29020fca80814448a0cfd
Justin Schildt <Just.schildt@gmail.com>
2023-06-19 01:56:17 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,123 @@
+# Flux SSE
+
+[![npm](https://img.shields.io/npm/v/flux-sse)](https://www.npmjs.com/package/flux-sse)
+[![npm bundle size](https://img.shields.io/bundlephobia/min/flux-sse)](https://www.npmjs.com/package/flux-sse)
+[![npm](https://img.shields.io/npm/dt/flux-sse)](https://www.npmjs.com/package/flux-sse)
+[![GitHub](https://img.shields.io/github/license/TheAyes/flux-sse)]()
+
+A middleware function that enables server-sent events (SSE) for your server. This middleware allows you to send real-time updates from the server to the client
+over a single HTTP connection. 🌐📡
+
+## Installation
+
+To use this middleware, install it using npm:
+
+```bash
+npm install flux-sse
+```
+
+## Usage
+
+Import the `useSSE` function from the module and add it as middleware to your Express application. Here's an example:
+
+```javascript
+import express from "express";
+import {useSSE} from "flux-sse";
+
+const app = express();
+
+// Add Flux SSE to specific routes
+app.get("/sse-events", useSSE, (req, res) => {
+	// Handle SSE events for this route
+});
+
+app.listen(3000, () => {
+	console.log("Server listening on port 3000");
+});
+```
+
+## Configuration Options
+
+The `useSSE` middleware function accepts a configuration object with the following options:
+
+| Option                 | Type         | Description                                                                                                                           |
+|------------------------|--------------|---------------------------------------------------------------------------------------------------------------------------------------|
+| `onClose`              | `() => void` | A callback function to be executed when the SSE connection is closed.                                                                 |
+| `heartbeatIntervalMs`  | `number`     | The interval (in milliseconds) at which a heartbeat event should be sent to keep the connection alive. Default: `10000` (10 seconds). |
+| `heartbeatCallback`    | `() => void` | A callback function to be executed each time a heartbeat event is sent.                                                               |
+| `event`                | `string`     | The default event type for SSE events.                                                                                                |
+| `retry`                | `number`     | The time (in milliseconds) that the client should wait before reconnecting if the connection is lost.                                 |
+| `id`                   | `string`     | The ID to include in SSE comments.                                                                                                    |
+| `bufferSize`           | `number`     | The maximum number of events to store in the event buffer before flushing.                                                            |
+| `throttleMs`           | `number`     | The minimum time (in milliseconds) between consecutive SSE events sent to the same client.                                            |
+| `maxRequestsPerSecond` | `number`     | The maximum number of SSE requests allowed per second from a single client.                                                           |
+
+## Extended Response Type
+
+The `SSEResponse` type extends the `ServerResponse` and `Response` types from the `http` and `express` modules, respectively. It includes additional methods for
+sending SSE events and managing subscriptions.
+
+### SSE Methods
+
+The `SSEResponse` object provides the following SSE-specific methods:
+
+#### `sseSend(data: unknown, options?: SSEOptions): void`
+
+Sends an SSE event with the specified `data` and `options`. The `data` can be any JavaScript object or value. The `options` parameter is an object that can
+contain the following properties:
+
+- `event` (optional): The event type for the SSE event.
+-
+
+`retry` (optional): The time (in milliseconds) that the client should wait before retrying the connection if an error occurs.
+
+- `id` (optional): The ID for the SSE event.
+- `eventId` (optional): The ID of the acknowledged event (to be used for acknowledging the event).
+
+#### `sseComment(comment: string): void`
+
+Sends an SSE comment with the specified `comment` string. SSE comments are non-event messages that can be used for various purposes.
+
+#### `sseAcknowledge(eventId: string): void`
+
+Acknowledges an SSE event with the specified `eventId`. This is useful for notifying the client that a particular event has been processed or received.
+
+#### `subscribeToEvent(event: string): void`
+
+Subscribes the client to the specified `event` type. Once subscribed, the client will receive SSE events of the subscribed type.
+
+#### `unsubscribeFromEvent(event: string): void`
+
+Unsubscribes the client from the specified `event` type. The client will no longer receive SSE events of the unsubscribed type.
+
+## Example
+
+Here's an example of how to use Flux SSE in an Express application:
+
+```javascript
+import express from "express";
+import {useSSE} from "flux-sse";
+
+const app = express();
+
+app.use(useSSE);
+
+app.get("/events", (req, res) => {
+	// Send SSE events to the client
+	res.sseSend({message: "Hello, world!"});
+});
+
+app.listen(3000, () => {
+	console.log("Server listening on port 3000");
+});
+```
+
+In the example above, the server sends an SSE event with the message "Hello, world!" to the client when the client requests the `/events` route.
+
+Feel free to customize the middleware configuration and SSE event handling according to your needs.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+🚀 Happy real-time communication with SSE! 🎉
---
+++ src/index.ts
@@ -0,0 +1,270 @@
+import {IncomingMessage, ServerResponse} from "http";
+import {NextFunction, Request, Response} from "express";
+
+/**
+ * Configuration options for SSE middleware.
+ */
+type SSEConfig = {
+	onClose?: () => void;
+	heartbeatIntervalMs?: number;
+	heartbeatCallback?: () => void;
+	event?: string;
+	retry?: number;
+	id?: string;
+	bufferSize?: number;
+	throttleMs?: number;
+	maxRequestsPerSecond?: number;
+};
+
+/**
+ * Extended response type with SSE-specific methods.
+ */
+type SSEResponse = ServerResponse &
+	Response & {
+		sseSend: (data: unknown, options?: SSEOptions) => void;
+		sseComment: (comment: string) => void;
+		sseAcknowledge: (eventId: string) => void;
+		subscribeToEvent: (event: string) => void;
+		unsubscribeFromEvent: (event: string) => void;
+	};
+
+/**
+ * Options for sending SSE data.
+ */
+type SSEOptions = {
+	event?: string;
+	retry?: number;
+	id?: string;
+	eventId?: string;
+};
+
+/**
+ * Type definition for the SSE middleware function.
+ */
+type UniversalSSEMiddleware = (
+	request: IncomingMessage | Request,
+	response: SSEResponse,
+	next: NextFunction | ((err?: any) => void),
+	config: SSEConfig
+) => void;
+
+/**
+ * Represents the subscribed event types for a client.
+ */
+type ClientSubscription = {
+	eventTypes: Set<string>;
+};
+
+/**
+ * Represents an acknowledged SSE event.
+ */
+type EventAcknowledgement = {
+	eventId: string;
+	acknowledged: boolean;
+};
+
+/**
+ * SSE middleware function to enable server-sent events.
+ *
+ * @param {IncomingMessage | Request} request - The incoming request object.
+ * @param {SSEResponse} response - The response object with SSE-specific methods.
+ * @param {NextFunction | ((err?: any) => void)} next - The next function or error handler.
+ * @param {SSEConfig} config - The configuration options for SSE middleware.
+ */
+const useSSE: UniversalSSEMiddleware = (
+	request: IncomingMessage | Request,
+	response: SSEResponse,
+	next: NextFunction | ((err?: any) => void),
+	config: SSEConfig
+) => {
+	// Unique client ID
+	const clientId = (Math.random() * 10000).toString();
+	// Set of client connections
+	const clientConnections = new Set<string>();
+	// Event buffer for storing SSE events
+	const eventBuffer: string[] = [];
+	// Map of client subscriptions
+	const clientSubscriptions = new Map<string, ClientSubscription>();
+	// Map of event acknowledgements
+	const eventAcknowledgements = new Map<string, EventAcknowledgement>();
+	// Last request time for rate limiting
+	let lastRequestTime: number | null = null;
+	// Counter for tracking requests
+	let requestCounter = 0;
+
+	/**
+	 * Checks if throttling should be applied based on the configured throttle interval and last request time.
+	 *
+	 * @returns {boolean} - True if throttling should be applied, false otherwise.
+	 */
+	const shouldThrottle = (): boolean => {
+		if (!config.throttleMs || !lastRequestTime) {
+			return false;
+		}
+		const elapsedTime = Date.now() - lastRequestTime;
+		return elapsedTime < config.throttleMs;
+	};
+
+	/**
+	 * Checks if rate limiting should be applied based on the configured maximum requests per second.
+	 *
+	 * @returns {boolean} - True if rate limiting should be applied, false otherwise.
+	 */
+	const shouldRateLimit = (): boolean => {
+		if (!config.maxRequestsPerSecond) {
+			return false;
+		}
+		return requestCounter >= config.maxRequestsPerSecond;
+	};
+
+	/**
+	 * Resets the request counter and updates the last request time.
+	 */
+	const resetRequestCounter = (): void => {
+		requestCounter = 0;
+		lastRequestTime = Date.now();
+	};
+
+	/**
+	 * Sends an SSE event with the specified data and options.
+	 *
+	 * @param {unknown} data - The data to send as SSE.
+	 * @param {SSEOptions} [options] - The options for the SSE event.
+	 */
+	const sendEvent = (data: unknown, options?: SSEOptions): void => {
+		if (shouldThrottle() || shouldRateLimit()) {
+			return;
+		}
+
+		const eventData = JSON.stringify(data);
+		const { event, retry, id, eventId } = options || {};
+
+		if (!event || isClientSubscribedToEvent(clientId, event)) {
+			if (event) eventBuffer.push(`event: ${event}`);
+			if (id) eventBuffer.push(`id: ${id}`);
+			eventBuffer.push(`data: ${eventData}`);
+			if (retry) eventBuffer.push(`retry: ${retry}`);
+			if (eventId) {
+				eventBuffer.push(`eventId: ${eventId}`);
+				acknowledgeEvent(eventId);
+			}
+
+			response.write(`${eventBuffer.join("\n")}\n\n`);
+			eventBuffer.length = 0;
+
+			requestCounter++;
+			if (shouldRateLimit()) {
+				setTimeout(resetRequestCounter, 1000); // Reset the request counter after 1 second
+			}
+		}
+	};
+
+	/**
+	 * Sends an SSE comment.
+	 *
+	 * @param {string} comment - The comment to send as SSE.
+	 */
+	const sendComment = (comment: string): void => {
+		response.write(`: ${comment}\n\n`);
+	};
+
+	/**
+	 * Acknowledges an SSE event with the specified event ID.
+	 *
+	 * @param {string} eventId - The event ID to acknowledge.
+	 */
+	const acknowledgeEvent = (eventId: string): void => {
+		const acknowledgement = eventAcknowledgements.get(eventId);
+		if (acknowledgement) acknowledgement.acknowledged = true;
+	};
+
+	/**
+	 * Subscribes the client to the specified event type.
+	 *
+	 * @param {string} eventType - The event type to subscribe to.
+	 */
+	const subscribeToEvent = (eventType: string): void => {
+		let subscription = clientSubscriptions.get(clientId);
+		if (!subscription) {
+			subscription = { eventTypes: new Set<string>() };
+			clientSubscriptions.set(clientId, subscription);
+		}
+		subscription.eventTypes.add(eventType);
+	};
+
+	/**
+	 * Unsubscribes the client from the specified event type.
+	 *
+	 * @param {string} eventType - The event type to unsubscribe from.
+	 */
+	const unsubscribeFromEvent = (eventType: string): void => {
+		const subscription = clientSubscriptions.get(clientId);
+		if (subscription) {
+			subscription.eventTypes.delete(eventType);
+			if (subscription.eventTypes.size === 0) {
+				clientSubscriptions.delete(clientId);
+			}
+		}
+	};
+
+	/**
+	 * Checks if the client is subscribed to the specified event type.
+	 *
+	 * @param {string} clientId - The client ID.
+	 * @param {string} eventType - The event type to check.
+	 * @returns {boolean} - True if the client is subscribed, false otherwise.
+	 */
+	const isClientSubscribedToEvent = (clientId: string, eventType: string): boolean => {
+		const subscription = clientSubscriptions.get(clientId);
+		return subscription?.eventTypes.has(eventType) ?? false;
+	};
+
+	// Assign SSE-specific methods to the response object
+	response.sseSend = sendEvent;
+	response.sseComment = sendComment;
+	response.sseAcknowledge = acknowledgeEvent;
+	response.subscribeToEvent = subscribeToEvent;
+	response.unsubscribeFromEvent = unsubscribeFromEvent;
+
+	// Set SSE headers
+	response.setHeader("Content-Type", "text/event-stream");
+	response.setHeader("Cache-Control", "no-cache");
+	response.setHeader("Connection", "keep-alive");
+	if (config.retry) response.setHeader("Retry", config.retry.toString());
+	if (config.id) response.sseComment(`id: ${config.id}`);
+
+	response.flushHeaders();
+
+	// Heartbeat interval for keeping the connection alive
+	const heartbeat = setInterval(() => {
+		response.write(":heartbeat\n\n");
+		if (config.heartbeatCallback) config.heartbeatCallback();
+	}, config.heartbeatIntervalMs || 10000);
+
+	// Handle request closure
+	request.on("close", () => {
+		clearInterval(heartbeat);
+		if (config.onClose) config.onClose();
+
+		if (clientConnections.has(clientId)) {
+			clientConnections.delete(clientId);
+			response.sseComment("Reconnecting...");
+		}
+	});
+
+	// Add client to connections set
+	clientConnections.add(clientId);
+
+	// Override sseSend to flush event buffer if it exceeds the buffer size
+	response.sseSend = ((sseSend) => (data: unknown, options?: SSEOptions) => {
+		if (config.bufferSize && eventBuffer.length >= config.bufferSize) {
+			response.write(`${eventBuffer.join("\n")}\n\n`);
+			eventBuffer.length = 0;
+		}
+		sseSend(data, options);
+	})(response.sseSend);
+
+	next();
+};
+
+export { useSSE };
